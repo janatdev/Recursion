cf09107804b4aacb4e9abb0198bd9f11aaf77799
Janita <janita_irfan7@yahoo.com>
2018-03-01 01:14:50 +0000

    factorial of number using recursive method

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    class Recursion
+    {
+        public int factorial(int num)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                result = factorial(num - 1) * num;
+                return result;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var n = new Recursion();
+            
+            Console.WriteLine("Factorial of 2 is : {0}", n.factorial(7));
+            Console.WriteLine("Factorial of 5 is : {0}", n.factorial(8));
+            Console.ReadLine();
+        }
+    }
+}
